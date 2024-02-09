f54b3600324b384706eb293e2321fb372c15a4ae
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-02-09 17:56:55 +0100

    Added Start Button functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Hello")
+const startButton = document.getElementById("start-button");
+const startMenu = document.getElementById("start-menu");
+
+const open = () => {
+	startMenu.style.display = "block";
+	console.log("opened");
+};
+
+const close = () => {
+	startMenu.style.display = "none";
+};
+
+startButton.addEventListener("click", open);
+startButton.addEventListener("blur", close);
